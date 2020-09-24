63b33afef95388f9e15327b7c8c19defa0565e31
David <daviegourevitch@gmail.com>
2020-09-23 22:15:09 -0400

    Updates

---
+++ precache-manifest.5387a94b7a28e6bfbc2fc11db1541725.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa96c20ec5be965990ac58c4add39f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d8db1241be61e8d701a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70354107f13931607d8d",
+    "url": "/static/js/2.860e4d6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.860e4d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8db1241be61e8d701a",
+    "url": "/static/js/main.367d3a27.chunk.js"
+  },
+  {
+    "revision": "08a987a893211a12b5f9",
+    "url": "/static/js/runtime-main.d9793403.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
